eb92c751226207165f2a080fd361a3e1dfa022d2
JanSeidel200 <91047952+JanSeidel200@users.noreply.github.com>
2023-12-20 23:12:50 +0100

    Add files via upload

---
+++ Game/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator anim;
+    private PlayerMovement pm;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        pm.enabled = false;
+        anim.SetTrigger("death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
